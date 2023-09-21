cf0c9d1d67969f57d85068d3b8b555945bfae634
Tomer Landesman <tomer.landesman@env0.com>
2023-09-21 11:21:20 +0300

    bucket

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+
+    }
+  }
+}
+
 resource "null_resource" "null" {
   count = 3
+}
+
+resource "aws_s3_bucket" "tomer-test-bucket" {
+  bucket = "tomer-test-bucket2"
+
 }
