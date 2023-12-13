8cf936495a5e67bebc992357a67cf910936ac0e2
Tomer Landesman <tomer.landesman@env0.com>
2023-12-13 17:45:17 +0200

    s

--- terragrunt/misc/module-resource/module/null-module/main.tf
+++ terragrunt/misc/module-resource/module/null-module/main.tf
@@ -1,6 +1,22 @@
 resource "null_resource" "null" {
 }
 
-output "null" {
-  value = "tomer531aa"
+output "1" {
+  value = "1"
 }
+
+output "3" {
+  value = true
+}
+
+output "4" {
+  value = 2
+}
+
+output "5" {
+  value = {
+    "a" = "b"
+  }
+}
+
+
