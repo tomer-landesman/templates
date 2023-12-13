d8e08cd924f72b188435b373ed50fe89fb4a002c
Tomer Landesman <tomer.landesman@env0.com>
2023-12-13 17:53:26 +0200

    s

--- terragrunt/misc/module-resource/module/null-module/main.tf
+++ terragrunt/misc/module-resource/module/null-module/main.tf
@@ -1,31 +1,3 @@
 resource "null_resource" "null" {
 }
 
-output "str" {
-  value       = "1"
-  description = "this is a string"
-}
-
-output "bool" {
-  value       = true
-  description = "this is a bool"
-
-}
-
-output "num" {
-  value = 2
-}
-
-output "obj" {
-  value = {
-    "a" = "b"
-  }
-}
-
-output "sens" {
-  value     = "sensor"
-  sensitive = true
-
-}
-
-
