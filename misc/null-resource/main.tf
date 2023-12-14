565150b7688825620acaa5837187232194fc1100
Tomer Landesman <tomer.landesman@env0.com>
2023-12-14 17:12:58 +0200

    s

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,3 +1,29 @@
-resource "null_resource" "null" {
-  count = 3
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "null_resource" "test" {
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
+output "bucket" {
+  value = aws_instance.example_server.id
+}
+
+output "tag" {
+  value = aws_instance.example_server.tags
+}
+
+output "this" {
+  value = null_resource.test.id
 }
