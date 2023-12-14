1d3a79cf9d275993494ff4b8eb2f416d552af277
Tomer Landesman <tomer.landesman@env0.com>
2023-12-14 18:46:16 +0200

    s

--- terragrunt/misc/module-resource/module/null-module/main.tf
+++ terragrunt/misc/module-resource/module/null-module/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "null_resource" "test" {
-  triggers = {
-    timestamp = "${timestamp()}"
-  }
-}
+# resource "null_resource" "test" {
+#   triggers = {
+#     timestamp = "${timestamp()}"
+#   }
+# }
 
 module "nuller" {
   source = "./module/nuller"
